565435c733a24f8095424a37302400522f9960e5
hamza2024web <hamzaakroubi1528@gmail.com>
2025-09-17 14:54:17 +0100

    creation of the class UserService for registre and create the methodes

---
+++ src/main/java/com/hamza/digitalbank/service/UserService.java
@@ -0,0 +1,52 @@
+package com.hamza.digitalbank.service;
+
+import com.hamza.digitalbank.domain.Account;
+import com.hamza.digitalbank.domain.User;
+import com.hamza.digitalbank.repository.AccountRepository;
+import com.hamza.digitalbank.repository.UserRepository;
+
+import java.util.Optional;
+import java.util.Random;
+import java.util.Scanner;
+
+public class UserService {
+    private final UserRepository userRepository;
+    private final AccountRepository accountRepository;
+
+    public UserService (UserRepository userRepository,AccountRepository accountRepository){
+        this.userRepository = userRepository;
+        this.accountRepository = accountRepository;
+    }
+
+    public void registerNewUser(Scanner scanner){
+        System.out.println("\\n--- Création d'un nouveau compte utilisateur ---");
+
+        System.out.print("Entrez votre nom complet : ");
+        String fullName = scanner.nextLine();
+
+        System.out.print("Entrez votre adresse email : ");
+        String email = scanner.nextLine();
+
+        Optional<User> existingUser = userRepository.findByEmail(email);
+        if (existingUser.isPresent()){
+            System.out.println("Erreur : Un utilisateur avec cet email existe déjà. Veuillez vous connecter.");
+            return;
+        }
+
+        System.out.print("Entrez votre addresse postale : ");
+        String addresse = scanner.nextLine();
+
+        System.out.print("Choisissez un mot de passe : ");
+        String password = scanner.nextLine();
+
+        User newUser = new User(fullName,email,addresse,password);
+        userRepository.save(newUser);
+
+        String accountId = "FR" + (100000 + new Random().nextInt(900000));
+        Account newAccount = new Account(accountId, newUser.getId());
+        accountRepository.save(newAccount);
+
+        System.out.println("\nFélicitations, " + fullName + " ! Votre compte a été créé avec succès.");
+        System.out.println("Votre numéro de compte est : " + accountId);
+    }
+}
