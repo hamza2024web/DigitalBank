9b4f0d7bfb070846211a0b4508f4b705bf0aca1f
hamza2024web <hamzaakroubi1528@gmail.com>
2025-09-17 11:28:26 +0100

    implement the methodes and write the body of each methode with collections and streams

--- src/main/java/com/hamza/digitalbank/domain/repository/InMemoryUserRepository.java
+++ src/main/java/com/hamza/digitalbank/domain/repository/InMemoryUserRepository.java
@@ -2,25 +2,28 @@ package com.hamza.digitalbank.domain.repository;
 
 import com.hamza.digitalbank.domain.User;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
 public class InMemoryUserRepository implements UserRepository {
 
+    private final Map<UUID,User> userStore = new HashMap<>();
 
     @Override
     public void save(User user) {
-
+        userStore.put(user.getId(),user);
     }
 
     @Override
     public Optional<User> findById(UUID id) {
-        return Optional.empty();
+        return Optional.ofNullable(userStore.get(id));
     }
 
     @Override
     public Optional<User> findByEmail(String email) {
-        return Optional.empty();
+        return userStore.values().stream().filter(user -> user.getEmail().equalsIgnoreCase(email)).findFirst();
     }
 }
 
